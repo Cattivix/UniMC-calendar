0f12dc659d16521c5b922fe3c1828ec8eaa0d302
Cattivix <cattivix@outlook.it>
2022-10-29 17:11:03 +0200

    managed to scrape correctly the day, month, time and location of single lection

--- main.py
+++ main.py
@@ -11,8 +11,16 @@ r = requests.get('https://docenti.unimc.it/s.pierosara/timetable/26244')
 soup = BeautifulSoup(r.content, 'html.parser')
 
 timetable = soup.find(id = "timetable")
-time = timetable.ul.li
-print(time[0])
+lections = timetable.find_all("li")
+
+for lection in lections:
+    date = lection.find(class_ = "date")
+    day = lection.find(class_ = "day-number").text.strip()
+    month = lection.find(class_ = "month").text.strip()
+
+    time = lection.find(class_ = "time").text
+    place = lection.find(class_ = "place").text
+    print(day, month, time, place)
 
 
 ##print (soup.prettify())
